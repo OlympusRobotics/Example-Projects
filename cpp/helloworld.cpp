e8bc471215cc38a9bfa6ce5b1717b88d0b46573e
Jason Wang <91506829+Leek727@users.noreply.github.com>
2022-09-26 21:23:52 -0400

    Update helloworld.cpp

--- cpp/helloworld.cpp
+++ cpp/helloworld.cpp
@@ -1,8 +1,8 @@
-// Your First C++ Program
-
+// this line allows us to use functions that get user input and show text
 #include <iostream>
 
+// the section of code that will first be run
 int main() {
-    std::cout << "Hello World!";
-    return 0;
+    std::cout << "Hello World!"; // prints out Hello World!
+    return 0; // tells the computer that the program ran successfully
 }
