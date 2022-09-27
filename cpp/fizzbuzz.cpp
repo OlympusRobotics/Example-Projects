8eddbb41de901eccb84d159ed416d34313fd65f9
StereotypicalBlobby <111467052+StereotypicalBlobby@users.noreply.github.com>
2022-09-26 21:00:48 -0400

    Create fizzbuzz.cpp

---
+++ cpp/fizzbuzz.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main(){
+  for(int i = 0; i < 15; i++){
+    if(i % 15 == 0) std::cout << "fizzbuzz" << std::endl;
+    else if(i % 5 == 0) std::cout << "buzz" << std::endl;
+    else if(i % 3 == 0) std::cout << "fizz" << std::endl;
+    else std::cout << i << std::endl;
+  }
+}
