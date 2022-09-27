004711b0837508b2e4a208194fe7ecb2e2d0efa1
Abdullah Mohammed <aamohammed4556@gmail.com>
2022-09-27 01:14:19 +0000

    ur mom

--- python/main.py
+++ python/main.py
@@ -1,4 +1,19 @@
 # this is how we put "comments" or code that won't run when you execute the program
+# if it's any color other than grey, it will run when you run the code.
+# To run the code, click the menu in the top left corner then go run > run without debugging
 # print hello world to the terminal
 print("hello world")
 
+# using the equals sign, we can put any value that we want into variable. 
+# this a number aka integer
+# all this is doing is storing the number for later use in our program aka referencing 
+a_number = 10
+# this is also a number but since it has a decimal point, we call it a float
+a_float = 10.5
+# If it is an integer, its just called an integer. If it has a decimal, then its called a float
+
+# Since we put the numbers into variables, we can use them later in our program
+# for example, here we can put that variable that we made on line 7 and 8 into the print command.
+# guess what this will output
+print(a_number) 
+print(a_float)
