255111d0ea398f73e252e31f7e0ba8b411b6e582
StereotypicalBlobby <111467052+StereotypicalBlobby@users.noreply.github.com>
2022-09-26 21:28:48 -0400

    Update helloworld.cpp

--- cpp/helloworld.cpp
+++ cpp/helloworld.cpp
@@ -1,8 +1,19 @@
-// this line allows us to use functions that get user input and show text
-#include <iostream>
+// Your First C++ Program
 
-// the section of code that will first be run
+#include <iostream> //allows us to output values (make them show up in the terminal)
+#include <string> //allows us to use strings (basic text inside "quotation marks")
+
+//this is the starting function of the program (literally the main function)
 int main() {
-    std::cout << "Hello World!"; // prints out Hello World!
-    return 0; // tells the computer that the program ran successfully
+    
+    /*std::cout is the most common way to generate outputs in cpp. basically, you just type "std::cout" and put two < signs.
+      
+    then, you type whatever you want to output. here, I'm outputting the string "hello world". 
+    
+    lastly, i have a semicolon. semicolons just tell the program that the statement/action is over and that it can move on to the next statement*/
+    
+    std::cout << "Hello World!";
+    
+    
+    return 0; //return 0 tells the program that everything was successful
 }
